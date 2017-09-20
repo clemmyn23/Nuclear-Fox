86143bfbe0180d98f3f42eef4d4cacb87a45a90f
Clement Ng <clementng23@gmail.com>
2017-09-20 12:08:36 +1000

    cpp notes

--- 2_notes/cpp/friend.cpp
+++ 2_notes/cpp/friend.cpp
@@ -2,8 +2,27 @@
 
 #include "friend.hpp"
 
-BestFriend::BestFriend(const int name, const int num) :
-num {num} {};
+// BestFriend::BestFriend(const int name, const int num) :
+// num {num} {
+//     // empty
+// };
+
+// BestFriend::BestFriend(BestFriend&) {};
+
+// class constructor definition here
+Num::Num(int v) : value {v} {};
+Num operator+ (Num& lhs, Num& rhs) {
+    return Num {lhs.value + rhs.value};
+};
+
+std::ostream& operator<<(std::ostream &os, Num &n) {
+    os << n.value;
+    return os;
+};
+
+
+
+
 
 
 
@@ -11,9 +30,17 @@ int main() {
 
     // BestFriend::BestFriend("aa", 1);
     // BestFriend b = BestFriend("bb", 2);
-    BestFriend a {2, 1};
+    // BestFriend a {2, 1};
+
+    Num n1 {1};
+    Num n2 {2};
+    Num n3 = n1 + n2;
+    Num n4 {};
 
 
-    std::cout << ' ' << '\n';
+    std::cout << "first " << n1 << '\n';
+    std::cout << "second " << n2 << '\n';
+    std::cout << "third " << n3 << '\n';
+    std::cout << "fourth " << n4 << '\n';
     return 0;
 };
--- 2_notes/cpp/friend.hpp
+++ 2_notes/cpp/friend.hpp
@@ -1,22 +1,39 @@
 #include <string>
+//
+// class BestFriend {
+// public:
+//
+//     // constructor
+//     BestFriend(const int name, const int num);
+//
+//     // copy constructor
+//     BestFriend(const BestFriend&);
+//
+//
+//     // methods
+//     int getNum() const;
+//
+//     // operators
+//     friend bool operator< (BestFriend &a, BestFriend &b);
+//
+// private:
+//     int num;
+//     int age;
+// };
+//
 
-class BestFriend {
+
+class Num {
 public:
 
-    // constructor
-    BestFriend(const int name, const int num);
+    // constructor declaration
+    Num(int v = 5);
 
-    // copy constructor
-    BestFriend(const BestFriend&);
-
-
-    // methods
-    int getNum() const;
-
-    // operators
-    friend bool operator< (BestFriend &a, BestFriend &b);
+    // operator overloads
+    // "friend" methods for accessing private class members
+    friend Num operator+ (Num& lhs, Num& rhs);
+    friend std::ostream& operator<< (std::ostream &os, Num &n);
 
 private:
-    int num;
-    int age;
+    const int value;
 };
